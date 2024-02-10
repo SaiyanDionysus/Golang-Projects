285c923a60c94182b3314edd1fba3dc091de1726
SaiyanDionysus <pusika97@gmail.com>
2024-02-10 22:20:56 +0300

    htmx and go

--- golang_htmx/main.go
+++ golang_htmx/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello world\n")
+		io.WriteString(w, r.Method)
+	}
+
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
