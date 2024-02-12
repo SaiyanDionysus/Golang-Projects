410a59aeeace6dc80d0c6711c02e17ba90361701
SaiyanDionysus <pusika97@gmail.com>
2024-02-12 10:46:10 +0300

    displaying data

--- golang_htmx/main.go
+++ golang_htmx/main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
+	"text/template"
 
 	"net/http"
 )
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
 	fmt.Println("Hello")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello world\n")
-		io.WriteString(w, r.Method)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
 	}
-
 	http.HandleFunc("/", h1)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//13:40
