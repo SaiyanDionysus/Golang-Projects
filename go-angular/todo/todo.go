e415609a40cb6c54c6653d4e817daae076e8dfb7
SaiyanDionysus <pusika97@gmail.com>
2024-03-13 13:19:31 +0300

    full-todo.go

--- go-angular/todo/todo.go
+++ go-angular/todo/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"errors"
 	"sync"
+
+	"github.com/rs/xid"
 )
 
 var (
@@ -26,4 +29,65 @@ type Todo struct {
 
 func Get() []Todo {
 	return list
+}
+
+func Add(message string) string {
+	t := newTodo(message)
+	mtx.Lock()
+	list = append(list, t)
+	mtx.Unlock()
+	return t.ID
+}
+
+func Delete(id string) error {
+	location, err := findTodoLocation(id)
+	if err != nil {
+		return err
+	}
+	removeElementByLocation(location)
+	return nil
+}
+
+func Complete(id string) error {
+	location, err := findTodoLocation(id)
+	if err != nil {
+		return err
+	}
+	setTodoCompleteByLocation(location)
+	return nil
+}
+
+func newTodo(msg string) Todo {
+	return Todo{
+		ID:       xid.New().String(),
+		Message:  msg,
+		Complete: false,
+	}
+}
+
+func findTodoLocation(id string) (int, error) {
+	mtx.RLock()
+	defer mtx.RUnlock()
+	for i, t := range list {
+		if isMatchingID(t.ID, id) {
+			return i, nil
+		}
+	}
+	return 0, errors.New("could not find todo based on id")
+}
+
+func removeElementByLocation(i int) {
+	mtx.Lock()
+	list = append(list[:i], list[i+1:]...)
+	mtx.Unlock()
+}
+
+func setTodoCompleteByLocation(location int) {
+	mtx.Lock()
+	list[location].Complete = true
+	mtx.Unlock()
+}
+
+func isMatchingID(a string, b string) bool {
+	return a == b
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module angular-golang
+
+go 1.21.5
+
+require github.com/rs/xid v1.5.0 // indirect
